6a0ca4e92da6d68186f7a189d828967a70a896f7
d0ntreadthis <tamara.jordan1@hotmail.com>
2015-10-24 13:00:52 +0100

    Initial commit - application layout

---
+++ main.py
@@ -0,0 +1,44 @@
+import kivy
+kivy.require('1.9.0')
+
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+class ActivityInput(TextInput):
+	pass
+
+class DeleteButton(Button):
+	# When pressed, this should delete and remove the activity layout it's in
+	# Have image of a bin on it
+	pass
+
+
+class ActivityLayout(BoxLayout):
+	# Contains a text input for the activity name, and a delete button
+	# Will be horizontal
+	pass
+
+
+class ActivitiesLayout(BoxLayout):
+	# Just a layout for all of the activity layouts to go
+	# Will be vertical
+	pass
+
+
+class MainWidget(BoxLayout):
+	'''Returned when the app is run'''
+	pass
+
+
+class MainApp(App):
+
+	def build(self):
+		#Window.size = (x, y)
+		return MainWidget()
+
+if __name__ == '__main__':
+	MainApp().run()
